7b1c198cb42d6c2cab932b5cebe09dc40eb29b2e
Pouria <pouriashiralipour@gmail.com>
2023-11-13 15:51:10 +0330

    add order models

--- store/models.py
+++ store/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     slug = models.SlugField()
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.PositiveIntegerField()
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
@@ -16,3 +17,18 @@ class Customer(models.Model):
     email = models.EmailField()
     phone_number = models.CharField(max_length=15)
     birth_day = models.DateField(null=True, blank=True)
+
+
+class Order(models.Model):
+    ORDER_STATUS_PAID = "p"
+    ORDER_STATUS_UNPAID = "u"
+    ORDER_STATUS_CANCELED = "c"
+    ORDER_STATUS = [
+        (ORDER_STATUS_PAID, "Paid"),
+        (ORDER_STATUS_UNPAID, "Unpaid"),
+        (ORDER_STATUS_CANCELED, "Canceled"),
+    ]
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=1, choices=ORDER_STATUS, default=ORDER_STATUS_UNPAID
+    )
